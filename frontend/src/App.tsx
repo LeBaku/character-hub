ce93699e9da670a82c119f4a64c570e26ce6b97e
Baku <jules.magyari@epitech.eu>
2026-01-21 11:15:20 +0100

    Fix Redirect to login if users doesn't have token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Character from './pages/Characters'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import RequireAuth from './components/RequireAuth';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Character from './pages/Characters';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/characters" element={<Character />} />
+
+      <Route path="/characters" element={<RequireAuth><Character /></RequireAuth>} />
+
       <Route path="*" element={<Navigate to="/login" />} />
     </Routes>
   )
---
+++ frontend/src/components/RequireAuth.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from 'react-router-dom';
+
+type Props = {
+    children: JSX.Element;
+};
+
+export default function RequireAuth({ children }: Props) {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+        return <Navigate to='/login' replace />;
+    }
+
+    return children;
+}
